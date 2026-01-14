38aefce322bf868e016df791ee6ce64a5177e593
gpwn145 <gpwn145@gmail.com>
2026-01-14 12:53:39 +0900

    [feat] 수도버튼 추가

---
+++ Assets/Script/WaterButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class WaterButton : MonoBehaviour
+{
+    [Header("물 오브젝트")]
+    [SerializeField] private GameObject _waterObject;
+    private Collider _waterCollider;
+
+    public bool IsPressed { get; set; }
+
+    private void Start()
+    {
+        _waterCollider = _waterObject.GetComponent<Collider>();
+        _waterCollider.enabled = false;
+        _waterObject.tag = "Water";
+        _waterObject.SetActive(false);
+    }
+
+    public void WaterOpen(bool isPressed)
+    {
+        _waterObject.SetActive(isPressed);
+        _waterCollider.enabled = isPressed;
+        Debug.Log("물 버튼 눌림");
+    }
+}
