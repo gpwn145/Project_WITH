537f2811ee0f3aca7f4cbdd619b151b0ffb0fb2b
gpwn145 <gpwn145@gmail.com>
2026-01-14 12:54:23 +0900

    [feat] 게임매니저 생성

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int DestroyJarNumber {  get; private set; }
+
+    private void Awake()
+    {
+        #region 싱글톤
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+        #endregion
+
+        DestroyJarNumber = 0;
+    }
+
+    private void Start()
+    {
+        Jar.OnDestroyJar += CountJar;
+    }
+
+    public void CountJar()
+    {
+        DestroyJarNumber++;
+    }
+
+
+}
