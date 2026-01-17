7b906df8f6d4a9bb4cf6808cd23dde71f5df06b5
gpwn145 <gpwn145@gmail.com>
2026-01-17 16:01:08 +0900

    [feat] 장애물 스크립트

---
+++ Assets/Script/Hurdle.cs
@@ -0,0 +1,52 @@
+﻿using Photon.Pun;
+using System.Collections;
+using UnityEngine;
+
+public class Hurdle : MonoBehaviourPunCallbacks
+{
+    [Header("이동 힘")]
+    [SerializeField, Range(5f, 20f)] private float _moveSpeed = 10f;
+    [Header("밀려나는 힘")]
+    [SerializeField, Range(0f, 10f)] private float _backForce = 3f;
+
+    private Rigidbody _rigidbody;
+    private bool isBack;
+    private bool isWaiting = false;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Hurdle" || collision.gameObject.tag == "Jar")
+        {
+            isBack = true;
+            Debug.Log("입력 비활성화");
+
+            if (isWaiting == true)
+            {
+                return;
+            }
+
+            BackMove(collision);
+            StartCoroutine(WaitTime());
+        }
+    }
+
+    private void BackMove(Collision collision)
+    {
+        Vector3 backward = collision.contacts[0].normal;
+
+        _rigidbody.linearVelocity = Vector3.zero;
+        _rigidbody.AddForce(backward * _backForce, ForceMode.Impulse);
+        isWaiting = true;
+    }
+
+    IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(1f);
+        isWaiting = false;
+    }
+}
