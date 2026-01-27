3dd8c64355fbcf8fc09662afcfe275eeee3d45b3
gpwn145 <gpwn145@gmail.com>
2026-01-27 15:28:44 +0900

    [feat] 프리펩- 에셋 추가

---
+++ Assets/Script/BackendManager.cs
@@ -0,0 +1,51 @@
+﻿using Firebase;
+using Firebase.Extensions;
+using UnityEngine;
+
+public class BackendManager : MonoBehaviour
+{
+    public static BackendManager Instance { get; private set; }
+
+    private FirebaseApp app;
+    public static FirebaseApp App => Instance.app;
+
+    private FirebaseApp auth;
+    public static FirebaseApp Auth => Instance.auth;
+
+    //private FirebaseDatabase database;
+    //public static FirebaseDatabase Database => Instance.database;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.Result == DependencyStatus.Available)
+            {
+                app = FirebaseApp.DefaultInstance;
+                auth = FirebaseApp.DefaultInstance;
+                //database = FirebaseApp.DefaultInstance;
+
+                Debug.Log("파이어베이스 체크 성공");
+            }
+            else
+            {
+                Debug.Log($"파이어 베이스 체크 실패 : {task.Result}");
+
+                app = null;
+                auth = null;
+                //database = null;
+            }
+        });
+    }
+}
